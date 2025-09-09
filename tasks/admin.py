33b732a27847452c0ee8a857cf46447bd0bcb491
Annette3125 <anetuks@gmail.com>
2025-09-09 11:02:11 +0300

    register to admin.py TagAdmin and TaskAdmin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Task, Tag
 
 # Register your models here.
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "status", "priority", "is_archived", "created_at")
+    list_filter = ("status", "priority", "is_archived", "created_at")
+    search_fields = ("title", "description")
+    filter_horizontal = ("assigned_to", "tags")
